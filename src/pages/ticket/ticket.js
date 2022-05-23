c5e74216c71be7a815bf10c0a2acdb317944759e
Ifemide <aib.ifemide@gmail.com>
2022-05-23 11:00:19 +0100

    saved from ifemide's side

--- src/pages/ticket/ticket.js
+++ src/pages/ticket/ticket.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import LayoutAdmin from "../../utils/layoutAdmin";
 import "./ticket.css";
 import Emma from "../../assets/image 14.png";
@@ -8,6 +8,37 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Ticket = () => {
+	const [newTicket, setNewTicket] = useState(false);
+	const [ticketDetails, setTicketDetails] = useState({
+		title: '',
+		amount: '',
+		description: ''
+	});
+
+	let ticketData = [
+		{
+			title: 'Regular',
+			amount: '5000',
+			description: 'Regular Lorem Ipsum'
+		},
+		{
+			title: 'VIP',
+			amount: '25000',
+			description: 'VIP Lorem Ipsum'
+		},
+	];
+
+	// const saveNewTicket = e => {
+	// 	e.preventDefault();
+	// 	ticketData[ticketData.length] = newTicket;
+	// 	console.log(ticketData)
+	// 	setNewTicket({
+	// 		title: '',
+	// 		amount: '',
+	// 		description: ''
+	// 	})
+	// }
+
 	const createTicket = (e) => {
 		let count = 0;
 		e.preventDefault();
@@ -44,72 +75,84 @@ const Ticket = () => {
 				<div className='ticket-details'>
 					<h2>Upload Faaji Details</h2>
 					<div className='ticket-details-body'>
-						<form onSubmit={createTicket}>
+						{/*  onSubmit={createTicket} */}
+						<form>
 							<div className='ticket-group'>
 								<div className='ticket-cont'>
 									<input type='date' name='date' />
-									<button>Add Ticket</button>
+									<button onClick={() => setNewTicket(!newTicket)}>
+										{ newTicket ? 'Close' : 'Show' } Ticket Form
+									</button>
 								</div>
 							</div>
-							<div className='ticket-group ticket-groups'>
-								<div className='ticket-cont1'>
+							{
+								ticketData.map((ticket, index) => (
+									<div className='ticket-group ticket-groups' key={index}>
+										<div className='ticket-cont1'>
+											<input
+												type='text'
+												placeholder='Ticket Title'
+												className='ticket-title'
+												name='ticketTitle1'
+												value={ticket.title}
+											/>
+											<input
+												type='text'
+												placeholder='Amount'
+												className='ticket-amount'
+												value={ticket.amount}
+											/>
+										</div>
+										<input
+											type='text'
+											placeholder='Description'
+											className='ticket-description'
+											value={ticket.description}
+										/>
+									</div>
+							))
+							}
+							{
+								newTicket && <> <div className='ticket-group ticket-groups'>
+									<div className='ticket-cont1'>
+										<input
+											type='text'
+											placeholder='Ticket Title'
+											className='ticket-title'
+											// value={ticketDetails.title}
+											// onChange={(e) => setTicketDetails({
+											// 	...ticketDetails,
+											// 	title: e.target.value
+											// })}
+										/>
+										<input
+											type='text'
+											placeholder='Amount'
+											className='ticket-amount'
+											// value={ticketDetails.amount}
+											// onChange={(e) => setTicketDetails({
+											// 	...ticketDetails,
+											// 	amount: e.target.value
+											// })}
+										/>
+									</div>
 									<input
 										type='text'
-										placeholder='Ticket Title'
-										className='ticket-title'
-										name='ticketTitle1'
+										placeholder='Description'
+										className='ticket-description'
+										// value={ticketDetails.description}
+										// onChange={(e) => setTicketDetails({
+										// 	...ticketDetails,
+										// 	description: e.target.value
+										// })}
 									/>
-									<input
-										type='text'
-										placeholder='Amount'
-										className='ticket-amount'
-									/>
-								</div>
-								<input
-									type='text'
-									placeholder='Description'
-									className='ticket-description'
-								/>
-							</div>
-							<div className='ticket-group ticket-groups'>
-								<div className='ticket-cont1'>
-									<input
-										type='text'
-										placeholder='Ticket Title'
-										className='ticket-title'
-									/>
-									<input
-										type='text'
-										placeholder='Amount'
-										className='ticket-amount'
-									/>
-								</div>
-								<input
-									type='text'
-									placeholder='Description'
-									className='ticket-description'
-								/>
-							</div>
-							<div className='ticket-group ticket-groups'>
-								<div className='ticket-cont1'>
-									<input
-										type='text'
-										placeholder='Ticket Title'
-										className='ticket-title'
-									/>
-									<input
-										type='text'
-										placeholder='Amount'
-										className='ticket-amount'
-									/>
-								</div>
-								<input
-									type='text'
-									placeholder='Description'
-									className='ticket-description'
-								/>
-							</div>
-							<button type='submit'>Submit</button>
+									</div>
+									<button type='submit' 
+									// onClick={() => saveNewTicket()}
+									>Save New Ticket</button>
+								</>
+							}
+							
 						</form>
 					</div>
 				</div>
