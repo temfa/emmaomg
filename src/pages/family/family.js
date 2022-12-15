fdbbf0b86bb922e122cdf1d4c48f2e9b433a2e0e
temfa <topeakinfe@gmail.com>
2022-12-15 01:30:18 +0100

    Done

--- src/pages/family/family.js
+++ src/pages/family/family.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./family.css";
 import FamilyPic from "../../assets/family.png";
 import Layout from "../../utils/layout";
