acb23a5318bd0f6c018e7ec7213044e737d4e48d
temfa <akinfetemitope5@gmail.com>
2023-05-30 08:56:00 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -24,23 +24,23 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="about" element={<About />} />
-        <Route path="music" element={<Music text="self" />} />
-        <Route path="listen" element={<Music text="band" />} />
-        <Route path="theohEmGeeband" element={<Band />} />
-        <Route path="brand" element={<Brand />} />
-        <Route path="mc" element={<Mc />} />
-        <Route path="birthday" element={<Birthday />} />
-        <Route path="online" element={<Online />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="family" element={<Family />} />
-        <Route path="emmaadmin" element={<Emmaadmin />} />
-        <Route path="faaji" element={<Faajiadmin />} />
-        <Route path="video" element={<Video />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/music" element={<Music text="self" />} />
+        <Route path="/listen" element={<Music text="band" />} />
+        <Route path="/theohEmGeeband" element={<Band />} />
+        <Route path="/brand" element={<Brand />} />
+        <Route path="/mc" element={<Mc />} />
+        <Route path="/birthday" element={<Birthday />} />
+        <Route path="/online" element={<Online />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/family" element={<Family />} />
+        <Route path="/emmaadmin" element={<Emmaadmin />} />
+        <Route path="/faaji" element={<Faajiadmin />} />
+        <Route path="/video" element={<Video />} />
         // <Route path="ticket" element={<Ticket />} />
-        <Route path="band-members" element={<BandMembers />} />
-        <Route path="emma" element={<Emma title="Login" text="Create Account" link="/emmaregister" />} />
-        <Route path="emmaregister" element={<Emma title="Register" text="Login" link="/emma" />} />
+        <Route path="/band-members" element={<BandMembers />} />
+        <Route path="/emma" element={<Emma title="Login" text="Create Account" link="/emmaregister" />} />
+        <Route path="/emmaregister" element={<Emma title="Register" text="Login" link="/emma" />} />
       </Routes>
     </div>
   );
